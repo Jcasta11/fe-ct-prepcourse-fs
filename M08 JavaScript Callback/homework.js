c6f68d18703960542b369f1f8326448f233190a9
Jcasta11 <juanjoc1126@gmail.com>
2023-08-23 16:38:25 -0500

    MO8

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -5,19 +5,22 @@ function mayuscula(nombre) {
    // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
    // [Ejemplo]: "mario" ----> "Mario".
    // Tu código:
+   return nombre[0].toUpperCase() + nombre.substring(1);
 }
 
 function invocarCallback(cb) {
    // Invoca/ejecuta el callback `cb`.
    // [NOTA]: no debes reotrnar nada.
-   // Tu código:
-}
-
+   // Tu código:  
+   cb();
+}   
 function operacionMatematica(num1, num2, cb) {
    // En este ejercicio recibirás dos números y un callback.
    // El callback realiza una operación matemática, por lo que necesita de los dos números.
    // Retorna el resultado del callback pasándole como parámetros los números.
    // Tu código:
+
+   return cb(num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
@@ -26,6 +29,12 @@ function sumarArray(arrayOfNumbers, cb) {
    // Este resultado debes pasárselo como argumento al callback recibido.
    // [NOTA]: no debes reotrnar nada.
    // Tu código:
+
+   var guardar = 0
+   for (i = 0; i < arrayOfNumbers.length; i++){
+      guardar = arrayOfNumbers[i] + guardar;
+   }
+      cb(guardar);
 }
 
 function forEach(array, cb) {
@@ -33,6 +42,10 @@ function forEach(array, cb) {
    // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
    // Debes pasarle el elemento como argumento al callback.
    // Tu código:
+
+   array.forEach(elem=>(
+      cb(elem)
+   ));
 }
 
 function map(array, cb) {
@@ -40,13 +53,27 @@ function map(array, cb) {
    // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
    // Retorna el nuevo arreglo.
    // Tu código:
+   var newA = [];
+   array.forEach(i=>(
+      newA.push(cb(i))
+   ))
+   return newA;
 }
 
 function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   var a = [];
+  for (const i of arrayOfStrings){
+   if (i[0]=== "a"){
+      a.push(i);
+      }
+   }
+   return a;
 }
+   
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
